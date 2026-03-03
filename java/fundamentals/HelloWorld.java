bdc27b00055ebf2fc399e756ce7da7e29d815a29
Sandeeptumu <tumusandeep0000@gmail.com>
2026-03-03 20:32:42 +0530

    Deleted java/fundamentals/HelloWorld.java

--- java/fundamentals/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.java.fundamentals;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        int a=10,b=11,c;
-        c=a+b;
-        System.out.println("sum is" +c);
-    }
-}
